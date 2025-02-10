65f63a4a6ad65999c19285f9a4c3b4071e6fbca7
Mycroft <mycroft180202@gmail.com>
2025-02-10 07:49:15 +0700

    add

---
+++ LoadDB - Copy (2)/Program.cs
@@ -0,0 +1,29 @@
+using LoadDB.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Prn2111Context>();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Departments}/{action=Index}/{id?}"
+    );
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
